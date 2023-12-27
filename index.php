b7ec11ca9a477e6fd6006ac7dde884059cc2c367
Ziemowit Gil <90980811+fundacja-feer-it@users.noreply.github.com>
2023-12-27 19:45:35 +0100

    first commiit
    ee

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Narzędzia informatyczne w FEER </title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+  <header>
+<img src="https://feer.org.pl/files/foto_mini/ban/320x180/banner-1.png"> <br>
+<h3> Narzędzia informatyczne wykorzystywane w FEER </h3> <br> 
+
+  </header>
+
+  <main>
+
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+
+			</div>
+			
+			<p>
+         Strona zawiera linki do narzędzi informatycznych wyokorzystywanych przez FEER. Znajdziesz tu m.in generator stopek czy system do generowania kont.			</p>
+			
+			<div class="row">
+				<div class="col-md-12">  
+   
+            <div class="list-group">
+                             <a href="https://gmail.com" class="list-group-item list-group-item-action ">Poczta elektroniczna w domenie <b> feer.org.pl </b>  <span class="badge bg-primary">Wymaga logowania kontem Google w domenie feer.org.pl</span>  </a>
+                             <a href="https://webmail.webh.email/" class="list-group-item list-group-item-action ">Poczta elektroniczna w domenie <b> fundacja-feer.pl </b>  <span class="badge bg-warning">Może wymagać logowania lub autoryzacji</span>  </a>
+                             <a href="http://storage.fundacja-feer.pl" class="list-group-item list-group-item-action"> Magazyn (dysk) do przechowywania dużych plików <b> STORAGE   </b>  <span class="badge bg-warning">Może wymagać logowania lub autoryzacji</span>  </a>
+
+               <a href="https://tools.fundacja-feer.pl/stopka" class="list-group-item list-group-item-action "> Generator stopek <span class="badge bg-success"> Dostęp bez logowania</span></a> 
+               <a href="https://feer.my.salesforce.com" class="list-group-item list-group-item-action"> CRM - zarządzanie organizacją  <span class="badge bg-danger"> Dostęp tylko dla administracji i Liderów</span></a>
+               <a href="https://wfirma.pl/logowanie" class="list-group-item list-group-item-action"> System płacowy i portal pracownika <span class="badge bg-primary">Wymaga logowania kontem Google w domenie feer.org.pl </span> </a>
+               <a href="https://uepik24.pl/users/login" class="list-group-item list-group-item-action"> System księgowy <span class="badge bg-danger">Dostęp tylko dla administracji </span> </a>
+               <a href="https://loginkrakow.hubkolektyw.pl/" class="list-group-item list-group-item-action">  HubKolektyw - system do obsługi korespondencji <span class="badge bg-danger">Dostęp tylko dla administracji </span> </a>
+               <a href="/helpdesk" class="list-group-item list-group-item-action ">HelpDesk - zgłaszanie problemów  <span class="badge bg-warning">Może wymagać logowania lub autoryzacji</span>  </a>
+               <a href="/helpdesk/scp" class="list-group-item list-group-item-action ">HelpDesk - administracja systemem  <span class="badge bg-danger"> Dostęp tylko dla administracji i działu IT</span>  </a>
+               <a href="http://bugtracker.fundacja-feer.pl/bt/" class="list-group-item list-group-item-action"> BugT - Śledzenie zmian w systemch IT  <span class="badge bg-danger"> Dostęp tylko dla administracji i działu IT</span> </a>
+            </div>
+ <br>
+  <center>
+               Twój adres IP: <?php echo $_SERVER['REMOTE_ADDR'];  ?> <br>
+            Systemy są monitorowane, nieuprawnione próby dostępu zostaną zgłoszone organom ścigania.	
+             <hr>
+            <b>  Fundacja Edukacji Empatii Rozwoju "FEER" </b>
+              <br> ul. Zamknięta 10 lok. 1.5, 30-554 Kraków <br>
+              NIP: 7343570539 KRS: 0000779281
+</center> 
+          
+            	</div> 
+				</div>
+				<footer>
+
+      
+        </footer>					
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+   
+
+ </div>
+  </div>
+  </main>
+  <footer>
+
+
+
+</footer>
+
+</div>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
+
